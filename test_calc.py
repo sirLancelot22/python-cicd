160deaa878ffa1b4db7d939dba1ef69b73a89e40
jerome <jpzi@proton.me>
2025-10-19 13:53:19 +0200

    modif

--- test_calc.py
+++ test_calc.py
@@ -1,4 +1,8 @@
 from calc import add
 
-result = add(2, 3)
-print("Le résultat de 2 + 3 est :", result)
+def test_add():
+    result = add(2, 3)
+    print("Le résultat de 2 + 3 est :", result)
+    assert result == 5
+
+test_add()
