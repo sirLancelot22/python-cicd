06126b3a32926cf204abcf5d4e937f8bb3540aed
jerome <jpzi@proton.me>
2025-10-19 15:34:17 +0200

    ajout CD

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e  # stoppe le script si une commande échoue
 
 echo "Déploiement en cours..."
-cd /home/jerome/python-cd/
+cd /home/jerome/python-cicd/
 git reset --hard main
 git pull origin main  # récupère la dernière version
 git reset --hard main
